75418a0682060684294be31121a813fca61a8da6
coder-srb <149040354+coder-srb@users.noreply.github.com>
2024-09-08 11:19:21 +0530

    adding SearchBar

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,4 +1,5 @@
 import './homePage.scss'
+import SearchBar from '../../components/searchBar/SearchBar.jsx';
 
 function HomePage() {
     return (
@@ -14,6 +15,21 @@ function HomePage() {
                         facilis id pariatur fugit quos laudantium temporibus dolor ea
                         repellat provident impedit!
                     </p>
+                    <SearchBar />
+                    <div className="boxes">
+                        <div className="box">
+                            <h1>16+</h1>
+                            <h2>Years of Experience</h2>
+                        </div>
+                        <div className="box">
+                            <h1>200</h1>
+                            <h2>Award Gained</h2>
+                        </div>
+                        <div className="box">
+                            <h1>1200+</h1>
+                            <h2>Property Ready</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="imageContainer">
