8335091ed856ee2287c255aa9695635ce2002383
coder-srb <149040354+coder-srb@users.noreply.github.com>
2024-09-08 22:47:23 +0530

    implementing react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
-import "./layout.scss"
-import Navbar from "./components/navbar/Navbar.jsx"
+import "./routes/layout/layout.scss"
+// import Navbar from "./components/navbar/Navbar.jsx"
 import HomePage from "./routes/homePage/homePage"
 
+import {    // https://reactrouter.com/en/main/start/overview
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import ListPage from "./routes/listPage/listPage.jsx";
+import Layout from "./routes/layout/layout.jsx";
+import SinglePage from "./routes/singlePage/SinglePage.jsx";
+
+
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/:id",  // any random string value or number 
+          element: <SinglePage />
+        }
+      ]
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  )
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     <HomePage />
+    //   </div>
+    // </div>                // all this now moved to the "layout.jsx"
+
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
